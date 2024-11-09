c8b3347a831f1d9a4de016776cf793db53685155
rajensen <rajensen@ethz.ch>
2024-11-09 23:06:37 +0100

    Added benchmark functionality

--- deep_learning_rl_sm/utils.py
+++ deep_learning_rl_sm/utils.py
@@ -1,6 +1,126 @@
 from deep_learning_rl_sm.environments import connect_four
 import torch
+import os
+import urllib
 
+env_dict = {"c4": connect_four.ConnectFour}
+
+act_dims = {"halfcheetah" : 6, "hopper" : 3, "walker2d": 6}
+state_dims = {"halfcheetah" : 17, "hopper" : 11, "walker2d": 17}
+
+BENCHMARK_LIST = ['halfcheetah-medium-v0','halfcheetah-medium-replay-v0','halfcheetah-medium-expert-v0',
+                  'walker2d-medium-v0','walker2d-medium-replay-v0','walker2d-medium-expert-v0',
+                  'hopper-medium-v0','hopper-medium-replay-v0','hopper-medium-expert-v0']
+
+DATASET_URLS = {
+    'maze2d-open-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/maze2d/maze2d-open-sparse.hdf5',
+    'maze2d-umaze-v1' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/maze2d/maze2d-umaze-sparse-v1.hdf5',
+    'maze2d-medium-v1' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/maze2d/maze2d-medium-sparse-v1.hdf5',
+    'maze2d-large-v1' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/maze2d/maze2d-large-sparse-v1.hdf5',
+    'maze2d-eval-umaze-v1' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/maze2d/maze2d-eval-umaze-sparse-v1.hdf5',
+    'maze2d-eval-medium-v1' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/maze2d/maze2d-eval-medium-sparse-v1.hdf5',
+    'maze2d-eval-large-v1' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/maze2d/maze2d-eval-large-sparse-v1.hdf5',
+    'maze2d-open-dense-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/maze2d/maze2d-open-dense.hdf5',
+    'maze2d-umaze-dense-v1' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/maze2d/maze2d-umaze-dense-v1.hdf5',
+    'maze2d-medium-dense-v1' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/maze2d/maze2d-medium-dense-v1.hdf5',
+    'maze2d-large-dense-v1' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/maze2d/maze2d-large-dense-v1.hdf5',
+    'maze2d-eval-umaze-dense-v1' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/maze2d/maze2d-eval-umaze-dense-v1.hdf5',
+    'maze2d-eval-medium-dense-v1' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/maze2d/maze2d-eval-medium-dense-v1.hdf5',
+    'maze2d-eval-large-dense-v1' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/maze2d/maze2d-eval-large-dense-v1.hdf5',
+    'minigrid-fourrooms-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/minigrid/minigrid4rooms.hdf5',
+    'minigrid-fourrooms-random-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/minigrid/minigrid4rooms_random.hdf5',
+    'pen-human-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/hand_dapg/pen-v0_demos_clipped.hdf5',
+    'pen-cloned-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/hand_dapg/pen-demos-v0-bc-combined.hdf5',
+    'pen-expert-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/hand_dapg/pen-v0_expert_clipped.hdf5',
+    'hammer-human-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/hand_dapg/hammer-v0_demos_clipped.hdf5',
+    'hammer-cloned-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/hand_dapg/hammer-demos-v0-bc-combined.hdf5',
+    'hammer-expert-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/hand_dapg/hammer-v0_expert_clipped.hdf5',
+    'relocate-human-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/hand_dapg/relocate-v0_demos_clipped.hdf5',
+    'relocate-cloned-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/hand_dapg/relocate-demos-v0-bc-combined.hdf5',
+    'relocate-expert-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/hand_dapg/relocate-v0_expert_clipped.hdf5',
+    'door-human-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/hand_dapg/door-v0_demos_clipped.hdf5',
+    'door-cloned-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/hand_dapg/door-demos-v0-bc-combined.hdf5',
+    'door-expert-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/hand_dapg/door-v0_expert_clipped.hdf5',
+    'halfcheetah-random-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco/halfcheetah_random.hdf5',
+    'halfcheetah-medium-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco/halfcheetah_medium.hdf5',
+    'halfcheetah-expert-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco/halfcheetah_expert.hdf5',
+    'halfcheetah-medium-replay-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco/halfcheetah_mixed.hdf5',
+    'halfcheetah-medium-expert-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco/halfcheetah_medium_expert.hdf5',
+    'walker2d-random-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco/walker2d_random.hdf5',
+    'walker2d-medium-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco/walker2d_medium.hdf5',
+    'walker2d-expert-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco/walker2d_expert.hdf5',
+    'walker2d-medium-replay-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco/walker_mixed.hdf5',
+    'walker2d-medium-expert-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco/walker2d_medium_expert.hdf5',
+    'hopper-random-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco/hopper_random.hdf5',
+    'hopper-medium-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco/hopper_medium.hdf5',
+    'hopper-expert-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco/hopper_expert.hdf5',
+    'hopper-medium-replay-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco/hopper_mixed.hdf5',
+    'hopper-medium-expert-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco/hopper_medium_expert.hdf5',
+    'ant-random-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco/ant_random.hdf5',
+    'ant-medium-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco/ant_medium.hdf5',
+    'ant-expert-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco/ant_expert.hdf5',
+    'ant-medium-replay-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco/ant_mixed.hdf5',
+    'ant-medium-expert-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco/ant_medium_expert.hdf5',
+    'ant-random-expert-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco/ant_random_expert.hdf5',
+    'antmaze-umaze-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/ant_maze_new/Ant_maze_u-maze_noisy_multistart_False_multigoal_False_sparse.hdf5',
+    'antmaze-umaze-diverse-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/ant_maze_new/Ant_maze_u-maze_noisy_multistart_True_multigoal_True_sparse.hdf5',
+    'antmaze-medium-play-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/ant_maze_new/Ant_maze_big-maze_noisy_multistart_True_multigoal_False_sparse.hdf5',
+    'antmaze-medium-diverse-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/ant_maze_new/Ant_maze_big-maze_noisy_multistart_True_multigoal_True_sparse.hdf5',
+    'antmaze-large-play-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/ant_maze_new/Ant_maze_hardest-maze_noisy_multistart_True_multigoal_False_sparse.hdf5',
+    'antmaze-large-diverse-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/ant_maze_new/Ant_maze_hardest-maze_noisy_multistart_True_multigoal_True_sparse.hdf5',
+    'antmaze-umaze-v2' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/ant_maze_v2/Ant_maze_u-maze_noisy_multistart_False_multigoal_False_sparse_fixed.hdf5',
+    'antmaze-umaze-diverse-v2' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/ant_maze_v2/Ant_maze_u-maze_noisy_multistart_True_multigoal_True_sparse_fixed.hdf5',
+    'antmaze-medium-play-v2' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/ant_maze_v2/Ant_maze_big-maze_noisy_multistart_True_multigoal_False_sparse_fixed.hdf5',
+    'antmaze-medium-diverse-v2' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/ant_maze_v2/Ant_maze_big-maze_noisy_multistart_True_multigoal_True_sparse_fixed.hdf5',
+    'antmaze-large-play-v2' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/ant_maze_v2/Ant_maze_hardest-maze_noisy_multistart_True_multigoal_False_sparse_fixed.hdf5',
+    'antmaze-large-diverse-v2' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/ant_maze_v2/Ant_maze_hardest-maze_noisy_multistart_True_multigoal_True_sparse_fixed.hdf5',
+    'flow-ring-random-v0': 'http://rail.eecs.berkeley.edu/datasets/offline_rl/flow/flow-ring-v0-random.hdf5',
+    'flow-ring-controller-v0': 'http://rail.eecs.berkeley.edu/datasets/offline_rl/flow/flow-ring-v0-idm.hdf5',
+    'flow-merge-random-v0':'http://rail.eecs.berkeley.edu/datasets/offline_rl/flow/flow-merge-v0-random.hdf5',
+    'flow-merge-controller-v0':'http://rail.eecs.berkeley.edu/datasets/offline_rl/flow/flow-merge-v0-idm.hdf5',
+    'kitchen-complete-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/kitchen/mini_kitchen_microwave_kettle_light_slider-v0.hdf5',
+    'kitchen-partial-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/kitchen/kitchen_microwave_kettle_light_slider-v0.hdf5',
+    'kitchen-mixed-v0' : 'http://rail.eecs.berkeley.edu/datasets/offline_rl/kitchen/kitchen_microwave_kettle_bottomburner_light-v0.hdf5',
+    'carla-lane-v0': 'http://rail.eecs.berkeley.edu/datasets/offline_rl/carla/carla_lane_follow_flat-v0.hdf5',
+    'carla-town-v0': 'http://rail.eecs.berkeley.edu/datasets/offline_rl/carla/carla_town_subsamp_flat-v0.hdf5',
+    'carla-town-full-v0': 'http://rail.eecs.berkeley.edu/datasets/offline_rl/carla/carla_town_flat-v0.hdf5',
+    'bullet-halfcheetah-random-v0': 'http://rail.eecs.berkeley.edu/datasets/offline_rl/bullet/bullet-halfcheetah_random.hdf5',
+    'bullet-halfcheetah-medium-v0': 'http://rail.eecs.berkeley.edu/datasets/offline_rl/bullet/bullet-halfcheetah_medium.hdf5',
+    'bullet-halfcheetah-expert-v0': 'http://rail.eecs.berkeley.edu/datasets/offline_rl/bullet/bullet-halfcheetah_expert.hdf5',
+    'bullet-halfcheetah-medium-expert-v0': 'http://rail.eecs.berkeley.edu/datasets/offline_rl/bullet/bullet-halfcheetah_medium_expert.hdf5',
+    'bullet-halfcheetah-medium-replay-v0': 'http://rail.eecs.berkeley.edu/datasets/offline_rl/bullet/bullet-halfcheetah_medium_replay.hdf5',
+    'bullet-hopper-random-v0': 'http://rail.eecs.berkeley.edu/datasets/offline_rl/bullet/bullet-hopper_random.hdf5',
+    'bullet-hopper-medium-v0': 'http://rail.eecs.berkeley.edu/datasets/offline_rl/bullet/bullet-hopper_medium.hdf5',
+    'bullet-hopper-expert-v0': 'http://rail.eecs.berkeley.edu/datasets/offline_rl/bullet/bullet-hopper_expert.hdf5',
+    'bullet-hopper-medium-expert-v0': 'http://rail.eecs.berkeley.edu/datasets/offline_rl/bullet/bullet-hopper_medium_expert.hdf5',
+    'bullet-hopper-medium-replay-v0': 'http://rail.eecs.berkeley.edu/datasets/offline_rl/bullet/bullet-hopper_medium_replay.hdf5',
+    'bullet-ant-random-v0': 'http://rail.eecs.berkeley.edu/datasets/offline_rl/bullet/bullet-ant_random.hdf5',
+    'bullet-ant-medium-v0': 'http://rail.eecs.berkeley.edu/datasets/offline_rl/bullet/bullet-ant_medium.hdf5',
+    'bullet-ant-expert-v0': 'http://rail.eecs.berkeley.edu/datasets/offline_rl/bullet/bullet-ant_expert.hdf5',
+    'bullet-ant-medium-expert-v0': 'http://rail.eecs.berkeley.edu/datasets/offline_rl/bullet/bullet-ant_medium_expert.hdf5',
+    'bullet-ant-medium-replay-v0': 'http://rail.eecs.berkeley.edu/datasets/offline_rl/bullet/bullet-ant_medium_replay.hdf5',
+    'bullet-walker2d-random-v0': 'http://rail.eecs.berkeley.edu/datasets/offline_rl/bullet/bullet-walker2d_random.hdf5',
+    'bullet-walker2d-medium-v0': 'http://rail.eecs.berkeley.edu/datasets/offline_rl/bullet/bullet-walker2d_medium.hdf5',
+    'bullet-walker2d-expert-v0': 'http://rail.eecs.berkeley.edu/datasets/offline_rl/bullet/bullet-walker2d_expert.hdf5',
+    'bullet-walker2d-medium-expert-v0': 'http://rail.eecs.berkeley.edu/datasets/offline_rl/bullet/bullet-walker2d_medium_expert.hdf5',
+    'bullet-walker2d-medium-replay-v0': 'http://rail.eecs.berkeley.edu/datasets/offline_rl/bullet/bullet-walker2d_medium_replay.hdf5',
+    'bullet-maze2d-open-v0': 'http://rail.eecs.berkeley.edu/datasets/offline_rl/bullet/bullet-maze2d-open-sparse.hdf5',
+    'bullet-maze2d-umaze-v0': 'http://rail.eecs.berkeley.edu/datasets/offline_rl/bullet/bullet-maze2d-umaze-sparse.hdf5',
+    'bullet-maze2d-medium-v0': 'http://rail.eecs.berkeley.edu/datasets/offline_rl/bullet/bullet-maze2d-medium-sparse.hdf5',
+    'bullet-maze2d-large-v0': 'http://rail.eecs.berkeley.edu/datasets/offline_rl/bullet/bullet-maze2d-large-sparse.hdf5',
+}
+def get_env_dims(env: str = "c4"):
+    envir = env_dict[env]()
+    return envir.state_dim, envir.action_dim
+
+def download_dataset_from_url(dataset_url) -> str:
+    dataset_filepath = "./"+dataset_url.split("/")[-1]
+    if not os.path.exists(dataset_filepath):
+        print('Downloading dataset:', dataset_url, 'to', dataset_filepath)
+        urllib.request.urlretrieve(dataset_url, dataset_filepath)
+    if not os.path.exists(dataset_filepath):
+        raise IOError("Failed to download dataset from %s" % dataset_url)
+    return dataset_filepath
 
 def extract_dataset(data):
     # below the assumed format for the elements/trajectories in the dataset
@@ -23,9 +143,11 @@ def extract_dataset(data):
         dones[-1][-1] = 1  # sequence only stops when game completed (in our cases so far)
     return time_steps, action_masks, actions, returns_to_go, rewards, states, dones
 
-
-def generate_data(batch_size=1000):
-    c4 = connect_four.ConnectFour()
+def generate_random_data(batch_size=1000, env : str = "c4"):
+    data_path = f'deep_learning_rl_sm/data/offline_{env}_data.pt'
+    if os.path.exists(data_path):
+        return data_path
+    c4 = env_dict[env]()
     data = c4.generate_seq(batch_size)
     time_steps, action_masks, actions, returns_to_go, rewards, states, traj_masks = extract_dataset(data)
 
@@ -73,7 +195,6 @@ def generate_data(batch_size=1000):
     returns_to_go = torch.stack(returns_to_go)
 
     # Save the data
-    data_path = 'data/offline_data.pt'
     torch.save({
         'states': states,
         'actions': actions,
@@ -85,23 +206,23 @@ def generate_data(batch_size=1000):
     }, data_path)
     return data_path
 
+def test_c4_generate():
+    dp = generate_random_data()
 
-dp = generate_data()
+    loaded_data = torch.load(dp)
 
-loaded_data = torch.load(dp)
-
-# Access individual items from the loaded dictionary
-s = loaded_data['states']
-a = loaded_data['actions']
-r = loaded_data['rewards']
-d = loaded_data['dones']
-t_s = loaded_data['time_steps']
-a_m = loaded_data['action_masks']
-r_t_g = loaded_data['returns_to_go']
-print(s.shape)  # state trajectories have one more timestep than the rest
-print(a.shape)
-print(r.shape)
-print(d.shape)
-print(t_s.shape)
-print(a_m.shape)
-print(r_t_g.shape)
+    # Access individual items from the loaded dictionary
+    s = loaded_data['states']
+    a = loaded_data['actions']
+    r = loaded_data['rewards']
+    d = loaded_data['dones']
+    t_s = loaded_data['time_steps']
+    a_m = loaded_data['action_masks']
+    r_t_g = loaded_data['returns_to_go']
+    print(s.shape)  # state trajectories have one more timestep than the rest
+    print(a.shape)
+    print(r.shape)
+    print(d.shape)
+    print(t_s.shape)
+    print(a_m.shape)
+    print(r_t_g.shape)
--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 import argparse
-
+import h5py
 import numpy as np
 import torch
 import wandb
@@ -7,66 +7,83 @@ from deep_learning_rl_sm.trainer.trainer import Trainer
 from deep_learning_rl_sm.neuralnets.minGRU_Reinformer import minGRU_Reinformer
 from deep_learning_rl_sm.neuralnets.lamb import Lamb
 from deep_learning_rl_sm.environments import connect_four
+from deep_learning_rl_sm.utils import download_dataset_from_url,BENCHMARK_LIST,generate_random_data,DATASET_URLS,get_env_dims,state_dims,act_dims
 
-# TODO generate our datasets separately so we only have to load them here! input format!
-env = connect_four.ConnectFour()
-# maybe push generate sequences into the trainer class somewhere
-data = torch.load("deep_learning_rl_sm/data/offline_data.pt")
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_type", choices=["reinformer"], default="reinformer")
+    parser.add_argument("--dataset", type=str,choices=BENCHMARK_LIST + ["c4"], default="c4")
+    parser.add_argument("--num_eval_ep", type=int, default=10)
+    parser.add_argument("--max_eval_ep_len", type=int, default=1000)
+    parser.add_argument("--context_len", type=int, default=5)
+    parser.add_argument("--n_blocks", type=int, default=4)
+    parser.add_argument("--embed_dim", type=int, default=256)
+    parser.add_argument("--n_heads", type=int, default=8)
+    parser.add_argument("--dropout_p", type=float, default=0.1)
+    parser.add_argument("--grad_norm", type=float, default=0.25)
+    parser.add_argument("--tau", type=float, default=0.99)
+    parser.add_argument("--batch_size", type=int, default=128)
+    parser.add_argument("--lr", type=float, default=1e-4)
+    parser.add_argument("--wd", type=float, default=1e-4)
+    parser.add_argument("--warmup_steps", type=int, default=5000)
+    parser.add_argument("--max_train_iters", type=int, default=10)
+    parser.add_argument("--num_updates_per_iter", type=int, default=5000)
+    parser.add_argument("--device", type=str, default="cuda")
+    parser.add_argument("--seed", type=int, default=2024)
+    parser.add_argument("--init_temperature", type=float, default=0.1)
+    parser.add_argument("--eps", type=float, default=1e-8)
+    # use_wandb = False
+    parser.add_argument("--use_wandb", action='store_true', default=False)
+    return parser.parse_args()
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--model_type", choices=["reinformer"], default="reinformer")
-parser.add_argument("--env", type=str, default=env)
-parser.add_argument("--dataset", type=str, default="medium")
-parser.add_argument("--num_eval_ep", type=int, default=10)
-parser.add_argument("--max_eval_ep_len", type=int, default=1000)
-parser.add_argument("--dataset_dir", type=str, default="data/d4rl_dataset/")
-parser.add_argument("--context_len", type=int, default=5)
-parser.add_argument("--n_blocks", type=int, default=4)
-parser.add_argument("--embed_dim", type=int, default=256)
-parser.add_argument("--n_heads", type=int, default=8)
-parser.add_argument("--dropout_p", type=float, default=0.1)
-parser.add_argument("--grad_norm", type=float, default=0.25)
-parser.add_argument("--tau", type=float, default=0.99)
-parser.add_argument("--batch_size", type=int, default=128)
-parser.add_argument("--lr", type=float, default=1e-4)
-parser.add_argument("--wd", type=float, default=1e-4)
-parser.add_argument("--warmup_steps", type=int, default=5000)
-parser.add_argument("--max_train_iters", type=int, default=10)
-parser.add_argument("--num_updates_per_iter", type=int, default=5000)
-parser.add_argument("--device", type=str, default="cuda")
-parser.add_argument("--seed", type=int, default=2024)
-parser.add_argument("--init_temperature", type=float, default=0.1)
-parser.add_argument("--eps", type=float, default=1e-8)
-# use_wandb = False
-parser.add_argument("--use_wandb", action='store_true', default=False)
-args = parser.parse_args()
-
-if args.use_wandb:
-    wandb.init(
-        name=args.env + "-" + args.dataset,
-        project="Reinformer",
-        config=vars(args)
-    )
-# TODO have discrete set based on environment choice instead of manual
-discrete = True
-# TODO explore different target entropies
-# entropy to encourage exploration in RL typically -action_dim for continuous actions and -log(action_dim) when discrete
-target_entropy = -np.log(np.prod(env.action_dim)) if discrete else -np.prod(env.action_dim)
-args = vars(args)
-model = minGRU_Reinformer(state_dim=env.state_dim, act_dim=env.action_dim, n_blocks=args["n_blocks"],
-                          h_dim=args["embed_dim"], context_len=args["context_len"], n_heads=args["n_heads"],
-                          drop_p=args["dropout_p"], init_tmp=args["init_temperature"],
-                          target_entropy=target_entropy)
-optimizer = Lamb(
-            model.parameters(),
-            lr=args["lr"],
-            weight_decay=args["wd"],
-            eps=args["eps"],
+def main():
+    
+    args = parse_args()
+    
+    dataset = args.dataset
+    if BENCHMARK_LIST.__contains__(dataset):
+        #Benchmark Envs
+        discrete = False
+        dataset_filepath = download_dataset_from_url(DATASET_URLS[dataset])
+        f = h5py.File(dataset_filepath,"r")
+        data = f[list(f.keys())[0]][()]
+        d_env = dataset.split("-")[0]
+        state_dim,action_dim = state_dims[d_env], act_dims[d_env]
+    else:
+        #Our Envs
+        discrete = True
+        dataset_filepath = generate_random_data(args.batch_size, args.dataset)
+        data = torch.load(dataset_filepath)
+        state_dim, action_dim = get_env_dims(args.dataset)
+        
+    if args.use_wandb:
+        wandb.init(
+            name= args.dataset,
+            project="Reinformer",
+            config=vars(args)
         )
-scheduler = torch.optim.lr_scheduler.LambdaLR(
-            optimizer,
-            lambda steps: min((steps + 1) / args["warmup_steps"], 1)
-        )
-# expect the next line to cause problems (need padding to make this work)
-trainer = Trainer(model=model, dataset=data, optimizer=optimizer, scheduler=scheduler, parsed_args=args)
-trainer.train(args)
+        
+    # TODO explore different target entropies
+    # entropy to encourage exploration in RL typically -action_dim for continuous actions and -log(action_dim) when discrete
+    target_entropy = -np.log(np.prod(action_dim)) if discrete else -np.prod(action_dim)
+    args = vars(args)
+    model = minGRU_Reinformer(state_dim=state_dim, act_dim=action_dim, n_blocks=args["n_blocks"],
+                            h_dim=args["embed_dim"], context_len=args["context_len"], n_heads=args["n_heads"],
+                            drop_p=args["dropout_p"], init_tmp=args["init_temperature"],
+                            target_entropy=target_entropy)
+    optimizer = Lamb(
+                model.parameters(),
+                lr=args["lr"],
+                weight_decay=args["wd"],
+                eps=args["eps"],
+            )
+    scheduler = torch.optim.lr_scheduler.LambdaLR(
+                optimizer,
+                lambda steps: min((steps + 1) / args["warmup_steps"], 1)
+            )
+    # expect the next line to cause problems (need padding to make this work)
+    trainer = Trainer(model=model, dataset=data, optimizer=optimizer, scheduler=scheduler, parsed_args=args)
+    trainer.train(args)
+
+if __name__ == "__main__":
+    main()
