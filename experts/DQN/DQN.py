470eeb13678d10d0a082113ab38986c18eb3601e
tgoppelsroed <tgoppelsroed@ethz.ch>
2024-12-10 16:47:03 +0100

    connect 4 multi-agent compatible (2 agents). Basic reward shaping (give -1 if opponent can win next move). DQN training adapted to these changes.

---
+++ experts/DQN/DQN.py
@@ -0,0 +1,93 @@
+import math
+
+from DQN_net import DQN_net
+import torch
+from torch.optim import Adam
+import torch.nn.functional as F
+import random
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+class DQN(object):
+    def __init__(self, batchsize, gamma, eps_start, eps_end, eps_decay, n_actions=7):
+        self.BATCH_SIZE = batchsize
+        self.GAMMA = gamma
+        self.EPS_START = eps_start
+        self.EPS_END = eps_end
+        self.EPS_DECAY = eps_decay
+
+        self.n_actions = n_actions
+
+        self.policy_net = DQN_net().to(device)
+        self.target_net = DQN_net().to(device)
+        self.target_net.load_state_dict(self.policy_net.state_dict())  # hard update
+        self.target_net.eval()
+
+        self.optimizer = Adam(self.policy_net.parameters())
+
+    def set_eval(self):  # sets networks to evaluation mode (faster)
+        # Sets the model in evaluation mode
+        self.policy_net.eval()
+        self.target_net.eval()
+
+    def set_train(self):  # sets networks to training mode (needed for learning but slower)
+        # Sets the model in training mode
+        self.policy_net.train()
+        self.target_net.train()
+
+    def update(self, transition_batch):
+        # TODO save action masks to replay buffer
+        self.set_train()
+
+        state_batch = torch.cat(transition_batch.state).to(device)
+        action_batch = torch.stack(transition_batch.action).to(device)
+        action_mask_batch = torch.cat(transition_batch.action_mask).to(device)
+        reward_batch = torch.stack(transition_batch.reward).to(device)
+        done_batch = torch.cat(transition_batch.done).to(device)
+        next_state_batch = torch.cat(transition_batch.next_state).to(device)
+        nxt_action_mask_batch = torch.cat(transition_batch.next_action_mask).to(device)
+
+        # adjust masks for softmax
+        action_mask_batch = torch.where(action_mask_batch == 1, 0, -1e9)
+        nxt_action_mask_batch = torch.where(nxt_action_mask_batch == 1, 0, -1e9)
+
+        # get Q-values
+        state_action_values = self.policy_net(state_batch, action_mask_batch).gather(1, action_batch)
+        next_state_values = self.target_net(next_state_batch, nxt_action_mask_batch).max(1)[0].detach()
+
+        # Compute the expected Q values
+        expected_state_action_values = (1 - done_batch) * next_state_values * self.GAMMA + reward_batch
+        expected_state_action_values = expected_state_action_values.unsqueeze(1)  # change shape for loss
+
+        # Compute MSE loss
+        loss = F.mse_loss(state_action_values, expected_state_action_values)
+        # Optimize the model
+        self.optimizer.zero_grad()
+        loss.backward()
+        # torch.nn.utils.clip_grad_norm(self.policy_net.parameters(), max_norm=10.0)
+        self.optimizer.step()
+
+    def select_action(self, state, steps_done, action_mask):
+        # action_mask contains 1s for all valid actions
+        sample = random.random()
+        # linear decay
+        eps_threshold = self.EPS_START - (self.EPS_START - self.EPS_END) * min(steps_done / self.EPS_DECAY, 1.0)
+        if sample > eps_threshold:
+            mask = torch.tensor([-1e9 if entry == 0 else 0 for entry in action_mask])
+            with torch.no_grad():
+                action_vector = self.policy_net(state, mask)
+                # print("SM_vec: " + str(action_vector))
+                return torch.argmax(action_vector, dim=-1).unsqueeze(-1)
+        else:
+            # valid action indices
+            indices = [index for index, val in enumerate(action_mask) if val == 1]
+            num_actions = self.n_actions - (7 - sum(action_mask))
+            actionNo = random.randrange(num_actions)
+            actionNo = torch.tensor([[indices[actionNo]]], device=device)
+            return actionNo
+
+    @staticmethod
+    def hard_update(target, source):
+        for target_param, param in zip(target.parameters(), source.parameters()):
+            target_param.data.copy_(param.data)
---
+++ experts/DQN/DQN_net.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+
+
+class DQN_net(nn.Module):
+
+    def __init__(self, output_size=7, h=6, w=7):
+        super(DQN_net, self).__init__()
+        self.flatten = nn.Flatten()
+        in_size = h * w
+        self.net = nn.Sequential(nn.Linear(in_size, 128), nn.ReLU(), nn.Linear(128, 128), nn.ReLU(),
+                                 nn.Linear(128, 128), nn.ReLU(), nn.Linear(128, output_size))
+
+    def forward(self, state, mask=None):
+        """print()
+        print("state", str(state))"""
+        action_vec = self.net(state)
+        """for param in self.net.parameters():
+            print(param)
+        print("action_vec: " + str(action_vec))"""
+        return torch.softmax(action_vec, dim=-1) if mask is None else torch.softmax(action_vec + mask, dim=-1)
