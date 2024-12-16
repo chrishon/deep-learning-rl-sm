6aa6c6f33c5c4309dd91eb9a80434ca5920fa7cf
WhyAreYouJay <117834927+WhyAreYouJay@users.noreply.github.com>
2024-12-16 09:25:55 +0100

    Renaming and opt. minGruCell

--- deep_learning_rl_sm/neuralnets/minGRU.py
+++ deep_learning_rl_sm/neuralnets/minGRU.py
@@ -6,24 +6,27 @@ from torch import nn
 def g(x):
     return torch.where(x >= 0, x + 0.5, torch.sigmoid(x))
 
-
+#@torch.compile
 def log_g(x):
     return torch.where(x >= 0, (F.relu(x) + 0.5).log(), -F.softplus(-x))
 
-
+#@torch.compile
 def parallel_scan_log(log_coefficients, log_values):
-    # log_coefficients: (batch_size, seq_len, input_size)
-    # log_values: (batch_size, seq_len + 1, input_size)
+    # log_coefficients: (batch_size, device, input_size)
+    # log_values: (batch_size, device + 1, input_size)
     a_star = F.pad(torch.cumsum(log_coefficients, dim=1), (0, 0, 1, 0))
     log_h0_plus_b_star = torch.logcumsumexp(log_values - a_star, dim=1)
     log_h = a_star + log_h0_plus_b_star
+    del log_coefficients,log_values
+    torch.cuda.empty_cache()
     return torch.exp(log_h)[:, 1:]
 
 class Conv1dLayer(Module):
-    def __init__(self, dim, kernel_size):
+    def __init__(self, dim, kernel_size, device):
         super().__init__()
         self.kernel_size = kernel_size
-        self.net = nn.Conv1d(dim, dim, kernel_size = kernel_size, groups = dim)
+        self.net = nn.Conv1d(dim, dim, kernel_size = kernel_size, groups = dim, device = device)
+    #torch.compile()
     def forward(self, x):
         x = x.transpose(1, 2) # b n d -> b d n
         x = F.pad(x, (self.kernel_size - 1, 0), value = 0.)
@@ -31,13 +34,13 @@ class Conv1dLayer(Module):
         return x.transpose(1, 2) # b d n -> b n d
 
 class minGRU(Module):
-    def __init__(self, dim, expansion_factor = 1.):
+    def __init__(self, dim,batch_size, device, expansion_factor = 1.):
         super().__init__()
         self.dim=dim
         self.exp_dim = int(dim * expansion_factor)
-        self.f_hidden = Linear(dim, self.exp_dim, bias=False)
-        self.f_z = Linear(dim, self.exp_dim, bias=False)
-        self.down_projection = Linear(self.exp_dim,dim, bias=False) if expansion_factor != 1 else nn.Identity()
+        self.log_h = log_g(torch.zeros((batch_size, 1, self.exp_dim), device = device))
+        self.f = Linear(dim, 2*self.exp_dim, bias=False, device = device)
+        self.down_projection = Linear(self.exp_dim,dim, bias=False, device = device) if expansion_factor != 1 else nn.Identity()
         # output of f_z can be viewed as the proportion of the info from the current timestep that is incorporated into
         # the next hidden state (for more info see paper "Were RNNs All We Needed?")
 
@@ -58,90 +61,53 @@ class minGRU(Module):
         # (i.e. long sequences more likely to result in numerical underflow)
         # by e.g. converting to log-values, summing and then exponentiating we achieve the same result as
         # multiplying the original values but with better numerical stability
-
-    def forward(self, x, h_prev=None):
-        # x: (batch_size, seq_len, input_size)
+    #@torch.compile
+    def forward(self, x:torch.Tensor, h0=None):
+        # x: (batch_size, device, input_size)
         # h_0: (batch_size, 1, hidden_size)
-        h0 = torch.zeros(x.shape[0], 1, self.exp_dim, device=x.device)
-        
-        k = self.f_z(x)
+        k,h_x = self.f(x).chunk(2,dim = -1)
         log_z = -F.softplus(-k)
         log_coeffs = -F.softplus(k)
-        log_h_prev = log_g(h_prev)
-        log_tilde_h = log_g(self.f_hidden(x))
-        h = parallel_scan_log(log_coeffs, torch.cat([log_h_0, log_z + log_tilde_h], dim=1))
-        return self.down_projection(h)
+        log_tilde_h = log_g(h_x)
+        return self.down_projection(parallel_scan_log(log_coeffs, torch.cat([self.log_h,log_tilde_h + log_z], dim=1)))
     
 class CausalDepthWiseConv1d(Module):
-    def __init__(self, dim, kernel_size):
+    def __init__(self, dim, kernel_size, device):
         """Simple sequential CONV1D net"""
         super().__init__()
         self.kernel_size = kernel_size
         self.net = nn.Sequential(
-            nn.Conv1d(dim, dim, kernel_size = kernel_size, groups = dim),
-            nn.Conv1d(dim, dim, kernel_size = 1)
+            nn.Conv1d(dim, dim, kernel_size = kernel_size, groups = dim, device = device),
+            nn.Conv1d(dim, dim, kernel_size = 1, device = device)
         )
+    #@torch.compile
     def forward(self, x):
         x = x.transpose(1, 2) # b n d -> b d n
         x = F.pad(x, (self.kernel_size - 1, 0), value = 0.)
         x = self.net(x)
-        return x.transpose(1, 2) # b d n -> b n d    
+        return x.transpose(1, 2) # b d n -> b n d
     
-class BlockV1(Module):
-    """This Version is what I understand to be one block in the minGRU architecture"""
-    def __init__(self,dim,n_layers,drop_p,kernel_size,expansion_factor, mult=4): 
-        """This Version is what I understand to be one block in the minGRU architecture"""
-        super().__init__()
-        self.n_layers = n_layers
-        self.conv = CausalDepthWiseConv1d(dim, kernel_size) #Conv1dLayer(dim,kernel_size)
-        self.ln1 = torch.nn.LayerNorm(dim)
-        self.mingru_layers =[]
-        for _ in range(n_layers):
-            self.mingru_layers += [minGRU(dim, expansion_factor), torch.nn.LayerNorm(dim)]
-        self.mod_l = nn.Sequential(*self.mingru_layers)
-        self.ln2 = torch.nn.LayerNorm(dim)
-        self.mlp = nn.Sequential(
-            nn.Linear(dim, mult * dim),
-            nn.ReLU(),#Reinformer uses GELU
-            nn.Linear(mult * dim, dim),
-            nn.Dropout(drop_p),
-        )
-    def forward(self,x): 
-        x = self.conv(x) + x #Residuals!
-        minGRU_input = self.ln1(x)
-        for i in range(self.n_layers):
-            x = self.mingru_layers[2*i](minGRU_input) + x #Residuals!
-            minGRU_input = self.mingru_layers[2*i+1](x) # LayerNorm before next layer
-        mlp_input = self.ln2(minGRU_input) #Continue with the MLP
-        x = self.mlp(mlp_input)+x#Residuals!
-        return x
-
-
-class BlockV2(Module):
+    
+class MinGRUCell(Module):
     """This Version corresponds to what has been done in https://github.com/lucidrains/minGRU-pytorch/"""
-    def __init__(self,dim,n_layers,drop_p,kernel_size,expansion_factor, mult=4):
+    def __init__(self,dim,n_layers,drop_p,kernel_size,expansion_factor,batch_size,device, mult=4):
         """This Version corresponds to what has been done in https://github.com/lucidrains/minGRU-pytorch/"""
         super().__init__()
-        self.seq = []
-        self.module_list = []
-        for i in range(n_layers):
-            self.conv = CausalDepthWiseConv1d(dim, kernel_size) #Conv1dLayer(dim,kernel_size)
-            self.ln1 = torch.nn.LayerNorm(dim)
-            self.min_gru = minGRU(dim, expansion_factor)
-            self.ln2 = torch.nn.LayerNorm(dim)
-            self.mlp = nn.Sequential(
-                nn.Linear(dim, mult * dim),
+        self.conv = CausalDepthWiseConv1d(dim, kernel_size, device = device) #Conv1dLayer(dim,kernel_size, device = device)
+        self.ln1 = torch.nn.LayerNorm(dim, device = device)
+        self.min_gru = minGRU(dim,batch_size,device,expansion_factor)
+        self.ln2 = torch.nn.LayerNorm(dim, device = device)
+        self.mlp = nn.Sequential(
+                nn.Linear(dim, mult * dim, device = device),
                 nn.ReLU(),#Reinformer uses GELU
-                nn.Linear(mult * dim, dim),
+                nn.Linear(mult * dim, dim, device = device),
                 nn.Dropout(drop_p),
             )
-            self.seq = self.seq + [self.conv, self.ln1, self.min_gru, self.ln2, self.mlp]
-            self.module_list.append((self.conv, self.ln1, self.min_gru, self.ln2, self.mlp))
-        self.seq = nn.Sequential(*self.seq)
-            
-    def forward(self,x): 
-        for conv, ln1, mingru, ln2, mlp in self.module_list:
-            x = conv(x) + x
-            x = mingru(ln1(x)) + x
-            x = mlp(ln2(x)) + x
-        return x
+    #@torch.compile
+    def forward(self,x):
+        residual = x
+        x = self.conv(x) + residual
+        x = self.ln1(x)
+        x = self.min_gru(x) + residual
+        x = self.ln2(x)
+        return self.mlp(x)
